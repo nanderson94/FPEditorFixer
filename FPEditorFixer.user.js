6fc01671d24f9ba383b118e682eabf7ff3a43875
Nicholas Anderson <nander13@gmu.edu>
2014-07-13 23:56:56 -0400

    Fix page number and CORS
    Don't know how I forgot about this.

--- FPEditorFixer.user.js
+++ FPEditorFixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Facepunch Editor Fixer
 // @namespace   https://nanderson.me/
-// @version     0.6
+// @version     0.7
 // @description Unicode have you feeling down? Not anymore!
 // @match       http://*facepunch.com/editpost.php*
 // @match       http://*facepunch.com/showthread.php*
@@ -32,24 +32,32 @@ $(function() {
   if (window.location.href.match('editpost') && localStorage['FP_EditFix']) {
     var postNum = parseInt(localStorage['FP_EditFix'], 10),
       LPPPP = 30, // LabPunch Posts Per Page
-      lpPage = Math.floor( postNum / LPPPP ),
+      // +1 because pages are not zero-indexed
+      lpPage = Math.floor( postNum / LPPPP ) + 1,
       threadId = $('.navbit:last > a').attr('href').match(/t=([0-9]+)/)[1],
       postId = window.location.href.match(/[0-9]+/);
     // Double check the editor, is it empty?
     if ($('#vB_Editor_001_editor').text().length == 0) {
       // So we probably encountered the unicode problem, let's fix it!
-      $.get('http://lab.facepunch.com/api/post/list/', {
-        threadid: threadId,
-        page: lpPage
-      }, function(data) {
-        for (var i=0,len=data['data']['posts'].length;i<len;i++) {
-          if (data['data']['posts'][i]['postid'] == postId) {
-            // Alright, we finally found the post!
-            CKEDITOR.instances['vB_Editor_001_editor'].setData(data['data']['posts'][i]['pagetext']);
-            break;
+      if (GM_xmlhttpRequest) {
+        // We need cross-domain requests
+        GM_xmlhttpRequest({
+          method:"GET",
+          url:"http://lab.facepunch.com/api/post/list/?threadid="+threadId+"&page="+lpPage,
+          onload:function(res) {
+            console.log(res);
+            var data = JSON.parse(res.responseText);
+            console.log(data);
+            for (var i=0,len=data['data']['posts'].length;i<len;i++) {
+              if (data['data']['posts'][i]['postid'] == postId) {
+                // Alright, we finally found the post!
+                CKEDITOR.instances['vB_Editor_001_editor'].setData(data['data']['posts'][i]['pagetext']);
+                break;
+              }
+            }
           }
-        }
-      });
+        })
+      }
     }
   }
 });
